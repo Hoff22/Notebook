3c67db657741f9211f281dbca9d37b627e3963df
Hoff22 <abithoff@gmail.com>
2025-06-22 13:57:45 -0300

    inv modulo and kmp

--- Math/Modulo Inverse (Division Modulo).cpp
+++ Math/Modulo Inverse (Division Modulo).cpp
@@ -28,3 +28,24 @@ long long modular_inverse(long long a, long long m) {
 
     return (x % m + m) % m;
 }
+
+
+/// -----
+
+// O(log_2(N))
+long long fast_pow(long long a, long long b, long long mod){
+    long long res = 1;
+    while(b){
+        if(b&1){
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res; 
+}
+
+// ONLY FOR PRIME MODULO
+long long modular_inverse(long long a, long long m){
+    return fast_pow(a, m-2, m) % m;
+}
---
+++ Strings/kmp.cpp
@@ -0,0 +1,9 @@
+vector<int> compute_pi(string s&){
+	vector<int> pi(s.size(), 0);
+	for(int i = 1, j = 0; i < s.size(); i++){
+		while(j > 0 and s[i] != s[j]) j = pi[j-1];
+		if(s[i] == s[j]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
